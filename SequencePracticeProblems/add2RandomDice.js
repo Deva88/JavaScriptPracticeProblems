4c12c81acc5832bde0207836e7e0724f019e31c0
Deva88 <devendra.raj.sdm@gmail.com>
2021-05-07 07:06:58 +0530

    Added:Random Number to get single Digit.

--- SequencePracticeProblems/add2RandomDice.js
+++
@@ -1,7 +0,0 @@
-/* Add two Random Dice Number and Print the Result */
-var randomNum1 = Math.floor(Math.random() * 6) +1;
-console.log("First Random Number = " + randomNum1);
-var randomNum2 = Math.floor(Math.random() * 6) +1;
-console.log("Second Random Number = " + randomNum2);
-    result = randomNum1+randomNum2;
-    console.log(result);
--- SequencePracticeProblems/diceNum1To6.js
+++
@@ -1,3 +0,0 @@
-/* Use Random to get Dice Number between 1 to 6 */
-var randomNum = Math.floor(Math.random() * 6) +1;
-    console.log(randomNum);
